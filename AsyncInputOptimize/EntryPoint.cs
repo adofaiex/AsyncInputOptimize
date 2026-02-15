395cb5da8542612f419d2bac1e3f890b862678dd
ModsTag <96873869+ModsTag@users.noreply.github.com>
2026-02-15 10:36:18 +0800

    Add files via upload

---
+++ AsyncInputOptimize/EntryPoint.cs
@@ -0,0 +1,61 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using static UnityModManagerNet.UnityModManager;
+
+namespace AsyncInputOptimize
+{
+    public static class EntryPoint
+    {
+        public static Harmony harmony;
+        public static bool cover_is_installer;
+        public static void Setup(ModEntry me)
+        {
+            harmony = new Harmony(me.Info.Id);
+            me.OnToggle = Toggle;
+            me.OnUpdate = Update;
+            me.OnGUI = GUI;
+            foreach (ModEntry modEntry in modEntries)
+            {
+                if ((modEntry.Info.DisplayName == "Cover" || modEntry.Info.Id == "Cover" || modEntry.Info.AssemblyName == "Cover.dll") && modEntry.Active)
+                {
+                    cover_is_installer = true;
+                    break;
+                }
+            }
+        }
+        public static bool Toggle(ModEntry me, bool a)
+        {
+            foreach (ModEntry modEntry in modEntries)
+            {
+                if ((modEntry.Info.DisplayName == "Cover" || modEntry.Info.Id == "Cover" || modEntry.Info.AssemblyName == "Cover.dll") && modEntry.Active)
+                {
+                    cover_is_installer = true;
+                    return true;
+                }
+            }
+            if (a)
+            {
+                harmony.PatchAll();
+            }
+            else
+            {
+                harmony.UnpatchAll();
+            }
+            return true;
+        }
+        public static void Update(ModEntry me, float dt)
+        {
+            AudioDSPManager.Update();
+        }
+        public static void GUI(ModEntry me)
+        {
+            GUILayout.Label("当前调整的偏移: ");
+            GUILayout.Label("  dspTime: " + AudioDSPManager.GetDSPTime());
+            GUILayout.Label("  cpy_dspTime: " + AudioDSPManager.cpy_dspTime);
+            if (cover_is_installer)
+            {
+                GUILayout.Label("你已经安装了Cover了 直接用Cover的就行了");
+            }
+        }
+    }
+}
---
+++ AsyncInputOptimize/Patch/__scrConductor.cs
@@ -0,0 +1,63 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace AsyncInputOptimize.Patch
+{
+    [HarmonyPatch(typeof(scrConductor), "Update")]
+    public static class __scrConductor
+    {
+        private static long Update_1()
+        {
+            AudioDSPManager.GetSystemTimePreciseAsFileTime(out long a);
+            return DateTime.Now.Ticks - DateTime.UtcNow.Ticks + a + 504911232000000000L;
+        }
+        private static double Update_2() => AudioDSPManager.GetDSPTime();
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler_Update(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            /*移除IL代码
+             *  (ulong)DateTime.Now.Ticks;
+             */
+            /*添加IL代码
+             *  Update_1();
+             */
+            /*修改IL代码
+             *  AudioSetting.dspTime;
+             *  
+             *  Update_2();
+             */
+            bool patch = false;
+            int skip = 0;
+            foreach (CodeInstruction ci in instructions)
+            {
+                if (patch)
+                {
+                    patch = false;
+                    yield return new CodeInstruction(OpCodes.Call, typeof(__scrConductor).GetMethod(nameof(Update_1), AccessTools.all));
+                }
+                if (skip > 0)
+                {
+                    skip--;
+                    continue;
+                }
+                if (ci.opcode == OpCodes.Call && ((MethodInfo)ci.operand).Name == "get_Now")
+                {
+                    skip = 3;
+                    patch = true;
+                    continue;
+                }
+                if (ci.opcode == OpCodes.Call && ((MethodInfo)ci.operand).Name == "get_dspTime")
+                {
+                    ci.operand = typeof(__scrConductor).GetMethod(nameof(Update_2), AccessTools.all);
+                }
+
+                // 其余返回
+                yield return ci;
+            }
+            yield break;
+        }
+    }
+}
