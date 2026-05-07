508913ca2bd497123919fe9c54f635ac0a39ca74
ModsTag <www.chingching3086@gmail.com>
2026-05-08 01:23:36 +0800

    fix gui

--- AsyncInputOptimize/EntryPoint.cs
+++ AsyncInputOptimize/EntryPoint.cs
@@ -102,21 +102,6 @@ namespace AsyncInputOptimize
             GUILayout.Label("音频缓冲大小");
             GUILayout.Label("音频采样率");
             GUILayout.Label("音频DSPTime精度");
-            GUILayout.Label("");
-
-            GUILayout.Label("插值线程");
-            GUILayout.BeginHorizontal();
-            GUILayout.Space(16);
-            GUILayout.BeginVertical();
-            {
-                GUILayout.Label("Mode");
-                GUILayout.Label("Mode");
-                GUILayout.Label("更新消耗时间");
-                GUILayout.Label("每秒更新次数");
-            }
-            GUILayout.EndVertical();
-            GUILayout.EndHorizontal();
-
             GUILayout.EndVertical();
 
             GUILayout.BeginVertical(new GUIStyle(GUIStyle.none) { normal = gss, hover = gss, focused = gss, active = gss, onNormal = gss, onHover = gss, onFocused = gss, onActive = gss, border = new RectOffset(4, 4, 4, 4) });
@@ -128,20 +113,6 @@ namespace AsyncInputOptimize
             GUILayout.Label("");
             GUILayout.Label("");
             GUILayout.Label("");
-
-            GUILayout.Label("");
-            GUILayout.BeginHorizontal();
-            GUILayout.Space(16);
-            GUILayout.BeginVertical();
-            {
-                GUILayout.Label("");
-                GUILayout.Label("");
-                GUILayout.Label("");
-                GUILayout.Label("");
-            }
-            GUILayout.EndVertical();
-            GUILayout.EndHorizontal();
-
             GUILayout.EndVertical();
 
             double sim_dsptime = DSPTimeInterpolation.dspTime;
@@ -157,29 +128,43 @@ namespace AsyncInputOptimize
             GUILayout.Label(buffer_size.ToString());
             GUILayout.Label(sample_rate.ToString());
             GUILayout.Label((buffer_size / (double)sample_rate).ToString("f17") + "s");
-            GUILayout.Label("");
-
-            GUILayout.Label("");
-            GUILayout.BeginHorizontal();
-            GUILayout.Space(16);
-            GUILayout.BeginVertical();
-            {
-                if (GUILayout.Button(WorkerThread._unsafeMode ? "<color=#3fffff><b>超高速更新 (<color=#ff0000><b>不安全</b></color>)</b></color>" : "超高速更新 (<color=#ff0000>不安全</color>)"))
-                {
-                    WorkerThread._unsafeMode = !WorkerThread._unsafeMode;
-                    WorkerThread._highMode = false;
-                }
-                if (GUILayout.Button(WorkerThread._highMode ? "<color=#3fffff><b>高速更新</b></color>" : "高速更新"))
-                {
-                    WorkerThread._unsafeMode = false;
-                    WorkerThread._highMode = !WorkerThread._highMode;
-                }
-                GUILayout.Label((WorkerThread.MainUpdate.Result.deltaTime / 10000f).ToString("F5") + "ms");
-                GUILayout.Label(WorkerThread.MainUpdate.Result.updatePerSecond.ToString().PadLeft(5));
-            }
             GUILayout.EndVertical();
+
+            GUILayout.FlexibleSpace();
             GUILayout.EndHorizontal();
 
+
+            GUILayout.Label("插值线程");
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(16);
+
+            GUILayout.BeginVertical();
+            GUILayout.Label("Mode");
+            GUILayout.Label("Mode");
+            GUILayout.Label("更新消耗时间");
+            GUILayout.Label("每秒更新次数");
+            GUILayout.EndVertical();
+
+            GUILayout.BeginVertical(new GUIStyle(GUIStyle.none) { normal = gss, hover = gss, focused = gss, active = gss, onNormal = gss, onHover = gss, onFocused = gss, onActive = gss, border = new RectOffset(4, 4, 4, 4) });
+            GUILayout.Label("");
+            GUILayout.Label("");
+            GUILayout.Label("");
+            GUILayout.Label("");
+            GUILayout.EndVertical();
+
+            GUILayout.BeginVertical();
+            if (GUILayout.Button(WorkerThread._unsafeMode ? "<color=#3fffff><b>超高速更新 (<color=#ff0000><b>不安全</b></color>)</b></color>" : "超高速更新 (<color=#ff0000>不安全</color>)"))
+            {
+                WorkerThread._unsafeMode = !WorkerThread._unsafeMode;
+                WorkerThread._highMode = false;
+            }
+            if (GUILayout.Button(WorkerThread._highMode ? "<color=#3fffff><b>高速更新</b></color>" : "高速更新"))
+            {
+                WorkerThread._unsafeMode = false;
+                WorkerThread._highMode = !WorkerThread._highMode;
+            }
+            GUILayout.Label((WorkerThread.MainUpdate.Result.deltaTime / 10000f).ToString("F5") + "ms");
+            GUILayout.Label(WorkerThread.MainUpdate.Result.updatePerSecond.ToString().PadLeft(5));
             GUILayout.EndVertical();
 
             GUILayout.FlexibleSpace();
