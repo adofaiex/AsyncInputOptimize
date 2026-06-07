71a9fbf77b617daa0000919b530a0f6b8cae9c39
ModsTag <www.chingching3086@gmail.com>
2026-06-07 19:11:36 +0800

    support r136

--- AsyncInputOptimize/Patch/__scnCalibration.cs
+++ AsyncInputOptimize/Patch/__scnCalibration.cs
@@ -4,6 +4,7 @@ using System.Reflection.Emit;
 
 namespace AsyncInputOptimize.Patch
 {
+#if ALPHA || BETA || RELEASE
     [HarmonyPatch]
     public static class __scnCalibration
     {
@@ -32,4 +33,34 @@ namespace AsyncInputOptimize.Patch
             yield break;
         }
     }
+#elif ALPHA_2_9_8_R136
+    [HarmonyPatch]
+    public static class __scrCalibrationPlanet
+    {
+        [HarmonyPatch(typeof(scrCalibrationPlanet), "Start")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler_Start(IEnumerable<CodeInstruction> instructions)
+        {
+            yield return new CodeInstruction(OpCodes.Ldc_I4_1);
+            yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PatchMidLayer), nameof(PatchMidLayer.Calibration)));
+            foreach (CodeInstruction ci in instructions)
+            {
+                yield return ci;
+            }
+            yield break;
+        }
+        [HarmonyPatch(typeof(scrCalibrationPlanet), "Quit")]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler_Quit(IEnumerable<CodeInstruction> instructions)
+        {
+            yield return new CodeInstruction(OpCodes.Ldc_I4_0);
+            yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(PatchMidLayer), nameof(PatchMidLayer.Calibration)));
+            foreach (CodeInstruction ci in instructions)
+            {
+                yield return ci;
+            }
+            yield break;
+        }
+    }
+#endif
 }
